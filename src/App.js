a79a59280eb772629d668b0af7532476392f520a
Sarah Scott <sarahoscott@gmail.com>
2017-10-29 21:26:21 +0100

    styling & about section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
-import pacific from './centrepacifique03.gif'
 import styled from 'styled-components'
-import ImageMapper from 'react-image-mapper'
 import content from "./content.json"
+import wave from "./wave_icon.png"
+import house from "./water_house_icon.png"
+import buds from "./buds.png"
+import science from "./science.png"
+import Map from "./Components/Map"
 import './App.css'
 
 import {
@@ -12,21 +15,48 @@ import {
   Link
 } from 'react-router-dom'
 
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-)
-
 const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-)
+  <div style={{marginLeft: 0, marginTop: 40, display: "flex", justifyContent: "center", fontFamily: "klima"}}>
+    <p style={{alignContent: "center", textAlign: "center", width: "70%"}}>
+    Welcome to our interactive map of Germany and Oceania, in line with Pacific focus and leadership of COP23. With this map we want to emphasize a positive message through talanoa or shared stories. Here, we visually connect the stories of divestment movements in Germany and the climate justice movements in the Pacific Islands and, as well as other global indigenous movements to protect the land and water.
+    <br/><br/>
+    <img src={wave} style={{width: 100}}/>
+    
+    <h1>“Our Sea of Islands” – Oceania</h1>    
+    “There is a gulf of difference between viewing the Pacific as ‘islands in a far sea’ and as ‘a sea of islands’. The first emphasises dry surfaces in a vast ocean far from the centres of power. When you focus this way you stress the smallness and remoteness of the islands. The second is a more holistic perspective in which things are seen in the totality of their relationships.”
+    “Oceania is vast, Oceania is expanding, Oceania is hospitable and generous, Oceania is humanity rising from the depths of brine and regions of fire deeper still, Oceania is us. We are the sea, we are the ocean, we must wake up to this ancient truth and together use it to overturn all hegemonic views that aim ultimately to confine us again, physically and psychologically, in the tiny spaces which we have resisted accepting as our sole appointed place, and from which we have recently liberated ourselves. We must not allow anyone to belittle us again, and take away our freedom.”
+    Epeli Hau‘ofa. 2008. “Our Sea of Islands”. We Are the Ocean: Selected Works. University of Hawai‘i Press. 
+    <br/><br/>
+    
 
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
+    <img src={buds} style={{width: 100}}/>    
+    
+    
+    <h1>Fiji’s Vision for Presidency of COP23 – Talanoa</h1>
+    “Fiji’s vision is for a Presidency that is transparent and inclusive of all, advances the Paris Agreement and accelerates climate action for vulnerable societies, drawing on our own experiences as a small island developing state in the Pacific.”
+    “To infuse COP23 with the Fijian “Bula Spirit” of inclusiveness, friendliness and solidarity and promote the Pacific concept of talanoa. This is a process of inclusive, participatory and transparent dialogue that builds empathy and leads to decision making for the collective good. It is not about finger pointing and laying blame but is about listening to each other, learning from each other, sharing stories, skills and experiences. By focusing on the benefits of action, this process will move the global climate agenda forward.”
+    <br/><br/>
+
+    <img src={house} style={{height: 70}}/>    
+    
+    
+    <h1>Indigenous Peoples and non-self-governing territories</h1>
+    Indigenous Peoples are the custodians of land and water throughout the world, but are not given much of an ability to speak in forums like the COP, instead they are represented through their colonial occupiers.
+    Among the Indigenous Peoples of the Americas, Africa, Asia, and Europe, there are various Pacific and Caribbean non-member states that are sidelined with their shared climate impacts. In our map we highlight several of these in their talanoa and welcome further submissions.
+    <br/><br/>
+    <img src={science} style={{height: 50}}/>    
+    
+    <h1>350 Pacific #HaveYourSei – Pacific Climate Warrior Declaration on Climate Change</h1>
+    Fossil Free is the divestment arm of 350.org and its climate activism. 350 Pacific and the Pacific Climate Warriors have a hashtag campaign #HaveYourSei and a declaration you can sign to share the significance of making a change in this Pacific COP.
+    Sign the Pacific Climate Warrior Declaration on Climate Change
+    “This year, with Fiji as the President of COP 23, the Pacific region will show the world what true climate leadership is. We will highlight for the world what climate change in the Pacific looks like and how inaction on climate change exacerbates that threat.”
+    “Climate change is real and impacting now, and it’s imperative that we stand up for the Pacific, and the global community, and act now to avoid further climate catastrophe. This COP should be about the people, not the profits and the polluters.”
+    
+    <br/><br/>
+    
+    [ wave icon by Vladimir Belochkin from the Noun Project]
+    
+    </p>
   </div>
 )
 
@@ -36,17 +66,20 @@ const Calendar = () => (
 </div>
 )
 
+const Home = () => (
+  <Map />
+)
+
 const App = () => (
   <Router>
     <div>
       <ul>
-        <li style={{marginRight: 20, marginLeft: -20}}><Link className="thing" to="/">From a Sea of Islands to the Rhineland(s)</Link></li>
-        <li style={{marginRight: 20}}><Link to="/about">About</Link></li>
+        <li style={{marginRight: 10, marginLeft: -20, fontSize: 25}}><Link to="/ffb.cop23">From a Sea of Islands to the Rhineland(s)</Link> • </li>
+        <li style={{marginRight: 10, fontSize: 25}}><Link to="/about">About</Link> • </li>
         <li><Link to="/calendar">COP23 Calendar</Link></li>
       </ul>
 
-
-      <Route exact path="/" component={Map}/>
+      <Route exact path="/ffb.cop23" component={Home}/>
       <Route path="/about" component={About}/>
       <Route path="/calendar" component={Calendar}/>
     </div>
@@ -56,138 +89,7 @@ export default App
 
 
 
-const Description = styled.div`
-  width: 700px;
-`
-
-const MAP = {name: "my-map",
-  areas: content.filter((story) => {
-    if (story.coords) {
-      return true
-    }
-    return false
-  }).map((story) => {
-    return { shape: "rect", coords: story.coords, _id: story._id }
-  })
-}
 
 
 
-
-class Map extends Component {
-
-  constructor(props) {
-    super(props)
-    this.state = { width: 0, height: 0, showMap: true, showOtherDiv: false, highlightedArea: null }
-  }
-
-  updateDimensions() {    
-    if(window.innerWidth < 500) {
-      this.setState({ width: 450, height: 102, showMap: false });
-    } else {
-      let update_width  = window.innerWidth;
-      let update_height = Math.round(update_width/1.8);
-      this.setState({ width: update_width, height: update_height, showMap: true });
-    }
-  }
-
-  componentDidMount() {
-    this.updateDimensions()
-    window.addEventListener("resize", this.updateDimensions.bind(this));    
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.updateDimensions.bind(this));
-  }
-
-  handleMouseEnter(event) {
-    const story = content[event._id]
-    if (!story) return
-    const areaName = story.title
-    const video = story.video
-    this.setState({ highlightedArea: story, showOtherDiv: true })
-  }
-
-  render() {
-
-    const containerStyle = {
-      flexDirection: "row",
-      marginTop: "50px",
-    }
-
-    const selectedStory = this.state.highlightedArea
-    
-    return (
-      <div className="App">      
-        <div style={{textAlign: "left", marginLeft: 20 }}>
-          <h2>hover over a point on the map, then scroll down</h2>
-          <div style={containerStyle}>
-            {
-              this.state.showMap && 
-              <ImageMapper width={1200} 
-                      src={pacific}
-                      map={MAP}
-                      onMouseEnter={this.handleMouseEnter.bind(this)}
-              />
-            }
-            { selectedStory && <StoryContent selectedStory={content.find((item)=> item._id === selectedStory._id)} video={this.state.video}/> }         
-          </div>
-        </div>
-      </div>
-    )
-  }
-}
-
-
-class StoryContent extends Component {
-
-  title() {
-    const story = this.props.selectedStory
-    if (story.link) {
-      return (
-        <a href={story.link}>{story.title}</a>
-      )
-    }
-    return (
-      <h1>{story.title}</h1>
-    )
-  }
-
-  render() {
-    const story = this.props.selectedStory
-    const video = story && story.video
-    if (!story) return null
-
-    const subtitleStyle = {
-      fontSize: 16,
-      fontStyle: "italic",
-    }
-
-    return (
-      <div style={{marginLeft: 20}}>
-        { this.title() }
-        { story.author && <h1 style={subtitleStyle}>{story.author}</h1> }
-        { story.description && <Description>{story.description}</Description> }
-        { video ? <YouTube video={video} autoplay="0" rel="0" modest="1" /> : <div style={{height: 500, width: 10}}/> }
-      </div>
-    )
-  }
-}
-
-class YouTube extends Component {
-  render() {
-      var videoSrc = "https://www.youtube.com/embed/" + 
-          this.props.video + "?autoplay=" + 
-          this.props.autoplay + "&rel=" + 
-          this.props.rel + "&modestbranding=" +
-          this.props.modest;
-      return (
-        <div className="container" style={{marginTop: "20px", marginBottom: "40px"}}>
-          <iframe className="player" type="text/html" width="640" height="390"
-    src={videoSrc}
-    frameBorder="0"/>
-        </div>
-      )
-    }
-}
 
